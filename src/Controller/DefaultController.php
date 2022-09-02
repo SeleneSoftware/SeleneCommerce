41bfd433225bf150e5c967a516b9a087373e81ad
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-09-02 10:13:26 -0400

    starting the featured products

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(ProductRepository $productRepo): Response
     {
+        // Currently, the featured products are just random products.
+        // I actually plan on having a more defined list.
         return $this->render('default/index.html.twig', [
+            'featured' => $prodRepo->findTenRand(),
             'controller_name' => 'DefaultController',
         ]);
     }
