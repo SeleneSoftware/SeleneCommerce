f9c925e25d6153e051701232a28b3a5ac4e386f4
Jason Marshall <JasonMarshallSelene@gmail.com>
2022-09-18 23:35:02 -0400

    got the subcategory page running

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,7 +15,23 @@ class CategoryController extends AbstractController
     public function index(Category $cat): Response
     {
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'category' => $cat,
+        ]);
+    }
+
+    #[Route('/{slug}/{sub}', name: 'subcategory')]
+    #[Entity('category', expr: 'repository.findOneBySlug(slug)')]
+    public function subcat(Category $cat, string $sub): Response
+    {
+        foreach ($cat->getSubcategories() as $subcat) {
+            if ($sub === $subcat->getSlug()) {
+                break;
+            }
+        }
+
+        return $this->render('category/subcat.html.twig', [
+            'category' => $cat,
+            'subcat' => $subcat,
         ]);
     }
 }
