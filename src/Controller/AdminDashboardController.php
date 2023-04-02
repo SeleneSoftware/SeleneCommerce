0d753b94c24a49dbe4aa042eaf21e1b91123bd30
Jason Marshall <JasonMarshallSelene@gmail.com>
2023-04-02 12:38:25 -0400

    WIP for the bundle

--- src/Controller/AdminDashboardController.php
+++ src/Controller/AdminDashboardController.php
@@ -2,11 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Location;
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use Selene\CMSBundle\Controller\Admin\DashboardController;
 
 class AdminDashboardController extends DashboardController
 {
+    public function configureMenuItems(): iterable
+    {
+        foreach (parent::configureMenuItems() as $item) {
+            yield $item;
+        }
+        yield MenuItem::linkToCrud('Products', 'fas fa-list', Product::class);
+        yield MenuItem::linkToCrud('Locations', 'fas fa-list', Location::class);
+    }
 }
